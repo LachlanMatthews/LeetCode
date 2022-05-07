6f52d0e209cd4d3346545ba7e14b1d864f4c2c51
Lachlan <lhamtthews.15@gmail.com>
2022-05-07 13:50:29 +1000

    Update TwoSum.py documentation

--- Submissions/TwoSum.py
+++ Submissions/TwoSum.py
@@ -18,7 +18,15 @@ class Solution(object):
         :rtype: List[int]
         """
 
+#https://leetcode.com/problems/two-sum/
+
 # twoSum() takes a list of integers and a target number.
 # The function finds two numbers in a list that will 
 # add up to the target and then returns the answer
 # in an integer list
+
+#---Constraints
+# 2 <= nums.length <= 10^4
+# -10^9 <= nums[i] <= 10^9
+# -10^9 <= target <= 10^9
+# Only one valid answer exists.
