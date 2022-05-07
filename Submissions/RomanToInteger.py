3304e625f8c779f2199127947d0ecfc4bc572126
Lachlan <Lachlan-Matthews@outlook.com>
2022-05-07 14:09:27 +1000

    Add RomanToInteger.py

---
+++ Submissions/RomanToInteger.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def romanToInt(self, s):
+        symbols = ['I','V','X','L','C','D','M']
+        values = [1,5,10,50,100,500,1000]
+        result = 0
+        for x in range(len(s)):
+            if x != len(s) - 1:
+                if symbols.index(s[x]) < symbols.index(s[x + 1]):
+                    result -= values[symbols.index(s[x])]
+                else:
+                    result += values[symbols.index(s[x])]
+            else:
+                result += values[symbols.index(s[x])]
+        return result
+
+# https://leetcode.com/problems/roman-to-integer/
+
+# romanToInt() takes a string that contains a roman
+# numeral and then converts it to the number that
+# it represents
+
+#---Constraints
+# 1 <= s.length <= 15
+# s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+# It is guaranteed that s is a valid roman numeral in the range [1, 3999].
